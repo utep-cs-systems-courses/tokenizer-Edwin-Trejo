ef664e6fd8c8cef0d896cc2a602f71391329e4cc
Edwin-Trejo <etrejo9@miners.utep.edu>
2021-09-27 10:51:32 -0600

    Added a while loop and a switch to uimain, work in progress

--- src/uimain.c
+++ src/uimain.c
@@ -5,8 +5,46 @@
 
 int main()
 {
+  char str[50];
+  puts("Welcome to the Tokenizer!");
+
+  while(1){
+    char str[50];
+    fputs("Select next action (Enter string = 'a', Show history 'h', Clear history 'c') or 'q' to quit\n> ", stdout);
+    fflush(stdout);
+    int c;
+    while((c = getchar()) == '\n'); /*ignore newlines*/
+    if (c == EOF)
+      goto done;
+
+    switch (c){
+    case 'a':
+      printf("You selected Enter string, please enter a string\n");
+      fgets(str,50,stdin);
+      printf("You entered:");
+      puts(str);
+      break;
+    case 'h':
+      puts("You selected Show history");
+      break;
+    case 'c':
+      puts("You selected clear history");
+      break;
+    case 'q':
+      puts("You selected QUIT");
+      goto done;
+    case '\n':
+      break;
+    default:
+      printf("Unrecognized option '%c', please try again\n", c);
+    }      
+  }
+ done:
+  return 0;
+}
+  /*
   char userInput[100];
-     /*
+  /*
   while(1){
     putchar('$');
     fgets(userInput,100,stdin);
@@ -14,7 +52,7 @@ int main()
     
   }
   
-  return 0;*/
+  return 0;
   char str[50];
   char *word = "hello world";
   printf("Tokenizer!\n");
@@ -27,12 +65,12 @@ int main()
   print_tokens(tokens);
   free_tokens(tokens);
   
-
+*/
  
  
 
   
  
-}
+
 
 
